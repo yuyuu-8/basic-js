8d5495f1ce60d420e7e62d23b8a1bb1bc5a22562
yuyu <sishichulu@gmail.com>
2025-01-07 00:44:41 +0300

    done count-cats, carbon-dating, dream-team, partially what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,27 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let activity = parseFloat(sampleActivity);
+  if (typeof sampleActivity !== 'string' || isNaN(activity) || activity <= 0 || activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+
+  const decayConstant = Math.LN2 / HALF_LIFE_PERIOD;
+  const age = Math.log(MODERN_ACTIVITY / activity) / decayConstant;
+
+  return Math.ceil(age);
 }
 
 module.exports = {
