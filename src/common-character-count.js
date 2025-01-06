abdd0c53e9d21c205d543d72fc7e85bbdbac1de4
yuyu <sishichulu@gmail.com>
2025-01-07 02:22:40 +0300

    done common characters count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const frequencyMap1 = createFrequencyMap(s1);
+  const frequencyMap2 = createFrequencyMap(s2);
+
+  let commonCount = 0;
+
+  for (const char in frequencyMap1) {
+    if (frequencyMap2[char]) {
+      commonCount += Math.min(frequencyMap1[char], frequencyMap2[char]);
+    }
+  }
+
+  return commonCount;
+}
+
+function createFrequencyMap(str) {
+  const frequencyMap = {};
+
+  for (const char of str) {
+    if (frequencyMap[char]) {
+      frequencyMap[char]++;
+    } else {
+      frequencyMap[char] = 1;
+    }
+  }
+
+  return frequencyMap;
 }
 
 module.exports = {
